7c885ad1539d515d7439c1c9e52628216295ff15
Ethan <ethan.bell@student.curtin.edu.au>
2018-09-30 21:07:01 +0800

    More graph code added

--- app/src/main/java/MedicalReview/DailyReview.java
+++ app/src/main/java/MedicalReview/DailyReview.java
@@ -1,5 +1,7 @@
 package MedicalReview;
 
+import android.provider.ContactsContract;
+
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
@@ -23,7 +25,8 @@ public class DailyReview {
 
     }
 
-    public boolean generateGraph(String[] data) {
+    //Map should have time as key(millis since epoch) and value(corresponding value)
+    public boolean generateGraph(Map<Long, Integer> data, String typeOfGraph) {
         boolean success = true;
 
 
@@ -38,7 +41,7 @@ public class DailyReview {
         Value should be the corresponding stoma state value (Integer from 1-10)
      */
     public boolean calcStateGraph(Map<Long, Integer> stateData) {
-        boolean success = false;
+        boolean success = true;
         Long[] attributes = stateData.keySet().toArray(new Long[stateData.size()]);
         DataPoint[] points = new DataPoint[attributes.length];
         int ii = 0;
@@ -57,11 +60,35 @@ public class DailyReview {
         formatter.setHorizontalLabels(HOURS);
         formatter.setVerticalLabels(new String[]{"1","2","3","4","5","6","7","8","9","10"});
 
-        //add the state names to the other y axis
+        //add the state names to the far side y axis
         StaticLabelsFormatter formatter2 = new StaticLabelsFormatter(graph);
         formatter2.setVerticalLabels(new String[]{"Green", "Yellow", "Red"});
         graph.getSecondScale().addSeries(series);
         graph.getSecondScale().setLabelFormatter(formatter2);
+
+        return success;
+    }
+
+    //calculate a graph with the stoma output volume
+    public boolean calcVolumeGraph(Map<Long, Integer> volData) {
+        boolean success = true;
+        Long[] attributes = volData.keySet().toArray(new Long[volData.size()]);
+        DataPoint[] points = new DataPoint[attributes.length];
+        int ii = 0;
+
+        for (Long key: attributes) {
+            points[ii] = new DataPoint(new Date(attributes[ii]), volData.get(key));   //data point with datetime and value
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
+
+        graph.addSeries(series);
+
+        //use static labels to keep graph bounds constant
+        StaticLabelsFormatter formatter = new StaticLabelsFormatter(graph);
+        formatter.setHorizontalLabels(HOURS);
+
 
         return success;
     }
