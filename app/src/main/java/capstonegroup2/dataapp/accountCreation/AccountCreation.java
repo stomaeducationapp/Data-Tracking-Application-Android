f93c4c3561389c11d9160609d96d86d042f901bc
Jeremy Dunnet <17160124@student.curtin.edu.au>
2018-10-08 14:46:32 +0800

    - Finished all activity specific functionality - Added comment walkthrough for Validation integration + Log in Integration - Fixed files I edited to make work

--- app/src/main/java/capstonegroup2/dataapp/accountCreation/AccountCreation.java
+++ app/src/main/java/capstonegroup2/dataapp/accountCreation/AccountCreation.java
@@ -3,18 +3,23 @@ package capstonegroup2.dataapp.accountCreation;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import capstonegroup2.dataapp.R;
 
 /* AUTHOR INFORMATION
  * CREATOR - Jeremy Dunnet 07/10/2018
- * LAST MODIFIED BY - Jeremy Dunnet 07/10/2018
+ * LAST MODIFIED BY - Jeremy Dunnet 08/10/2018
  */
 
 /* CLASS/FILE DESCRIPTION
@@ -23,7 +28,8 @@ import capstonegroup2.dataapp.R;
  */
 
 /* VERSION HISTORY
- * 24/08/2018 - Created Activity design layout and added rudimentary code to display back for demo
+ * 07/10/2018 - Created Activity design layout and added rudimentary code to display back for demo
+ * 08/10/2018 - Finished up functionality related to activity (data collection and initial checks) and added draft calls to validation
  */
 
 /* REFERENCES
@@ -34,14 +40,24 @@ import capstonegroup2.dataapp.R;
  * Getting text values from views learned from https://www.mkyong.com/android/android-radio-buttons-example/
  * Getting string from strings.xml in code learned from https://stackoverflow.com/questions/7213924/access-string-xml-resource-file-from-java-android-code
  * Setting the spinner prompt learned from https://stackoverflow.com/questions/6385959/how-to-set-a-title-for-spinner-which-is-not-selectable
- * Alert dialog creation copied/adapted/learned from a previous Android implementation of my own (Jeremy Dunnet) - using the basics from debeloper.android.com
+ * Alert dialog creation copied/adapted/learned from a previous Android implementation of my own (Jeremy Dunnet) - using the basics from developer.android.com
+ * Moving EditText cursor learned from https://stackoverflow.com/questions/6217378/place-cursor-at-the-end-of-text-in-edittext
+ * Showing error popups in an EditText learned from https://stackoverflow.com/questions/18225365/show-error-on-the-tip-of-the-edit-text-android
+ * Idea on how to change text from TextWatcher learned from https://stackoverflow.com/questions/9498155/how-to-update-the-same-edittext-using-textwatcher/9498218
+ * Event listeners for key presses learned from https://stackoverflow.com/questions/4282214/onkeylistener-not-working-on-virtual-keyboard and https://developer.android.com/reference/android/text/TextWatcher.html#onTextChanged(java.lang.CharSequence,%20int,%20int,%20int)
+ * Chopping strings using substring learned from https://javarevisited.blogspot.com/2016/03/how-to-remove-first-and-last-character-from-String-in-java-example.html
+ * Fixing a String.matches result learned from https://stackoverflow.com/questions/20165485/why-is-string-matches-returning-false-in-java
+ * Creating a password checker elements adapted from https://github.com/yesterselga/password-strength-checker-android
+ * Colouring a progress bar learned from https://stackoverflow.com/questions/2020882/how-to-change-progress-bars-progress-color-in-android
+ * Placing progress bar next to an element learned from https://stackoverflow.com/questions/34915468/display-progress-bar-beside-the-text-in-listview
+ * Removing error symbol from EditText learned from https://stackoverflow.com/questions/10206799/remove-error-from-edittext
+ * Allowing errors on non-EditText elements learned from https://stackoverflow.com/questions/13508270/android-seterrorerror-not-working-in-textview
  * And many more from https://developer.android.com
  */
 
 public class AccountCreation extends Activity {
 
     //Classfields
-    //All in the layout we need to access later on
     //EditTexts for the user input sections
     private EditText unameInput;
     private EditText passInput;
@@ -49,27 +65,135 @@ public class AccountCreation extends Activity {
     private Spinner gameInput;
     //Checkbox we use for notifications settings
     private CheckBox notInput;
+    //Progress bar for password strength
+    private ProgressBar strengthBar;
+    //TextView fro password strength in english
+    private TextView passStrength;
+    //TextView for export - for error state
+    private TextView exportText;
+
+    //Boolean for text change
+    private Boolean unKeyCheck;
+    private Boolean pKeyCheck;
 
     //Values for any options that are set via clicks
     private String exportValue;
 
+    //Constants - Change here to ease refactoring
+    //Maximum length of username
+    public static final int UNAME_MAX = 20;
+
+
+    /* FUNCTION INFORMATION
+     * NAME - onCreate
+     * INPUTS - savedInstanceState
+     * OUTPUTS - none
+     * PURPOSE - This is the standard function to imitate creation of the activity this java code is tied to
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
 
+        //Find all the objects we need
         unameInput = findViewById(R.id.acUNInput);
         passInput = findViewById(R.id.acPassInput);
         gameInput = findViewById(R.id.acGameSett);
         notInput = findViewById(R.id.acNotSett);
-        gameInput.setPrompt(getString(R.string.ac_gamification_option_title));
+        passStrength = findViewById(R.id.acPassStrength);
+        strengthBar = findViewById(R.id.acStrengthBar);
+        exportText = findViewById(R.id.acExportTitle);
+
+        //gameInput.setPrompt(getString(R.string.ac_gamification_option_title));
+
+        //Set intial values to false - with nothing in the boxes yet they haven't been checked
+        unKeyCheck = false;
+        pKeyCheck = false;
+
+        //Set an TextWatch listener for each of the two text inputs so that we can do some per character analysis
+        //Username listener
+        TextWatcher unInputTextWatcher = new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                String text  = s.toString(); //Get the string
+                //If the text in the box is empty - don't bother with checking the characters inside
+                if((text.length() - 1) >= 0)
+                {
+                    //Check for violating length limit
+                    if(text.length() > UNAME_MAX)
+                    {
+                        //Remove the added character
+                        unameInput.setText((text.substring(0, (text.length() - 1))));
+                        //Display warning to the user
+                        unameInput.setError("Usernames have a maximum length of 20 characters");
+                        unameInput.setSelection((unameInput.getText().length())); //Place their cursor to the end of the line
+                    }
+                    else
+                    {
+                        //If there is an unaccepted character in the string
+                        if (text.matches(".*[^a-zA-Z0-9_].*") && (unKeyCheck == false))
+                        {
+                            //Remove all matches to this regex
+                            text = text.replaceAll("[^a-zA-Z0-9_]", "");
+                            unameInput.setText(text); //Replace text to corrected string
+                            //Display warning to the user
+                            unameInput.setError("Usernames can only have A-Z, 0-9 and _\nSpecial Characters are not permitted");
+                            unameInput.setSelection((unameInput.getText().length()));
+                            unKeyCheck = true; //Say we have changed a key (since by editing the string we may call this method recursively)
+                        }
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            { }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                unKeyCheck = false; //Since a key has changed - we may need to edit it
+            }
+        };
+        TextWatcher pInputTextWatcher = new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                String text  = s.toString(); //Get the string
+                //If the text in the box is empty - don't bother with checking the characters inside
+                if((text.length() - 1) >= 0)
+                {
+                        //If there is an unaccepted character in the string
+                        if (text.matches(".*[^a-zA-Z0-9_].*") && (pKeyCheck == false))
+                        {
+                            //Remove all matches to this regex
+                            text = text.replaceAll("[^a-zA-Z0-9_]", "");
+                            passInput.setText(text); //Replace text to corrected string
+                            //Display warning to the user
+                            passInput.setError("Passwords can only have A-Z, 0-9 and _\nSpecial Characters are not permitted");
+                            passInput.setSelection((passInput.getText().length()));
+                            pKeyCheck = true; //Say we have changed a key (since by editing the string we may call this method recursively)
+                        }
+                        else //We only want one error at a time - The bad character takes precedence first since if we remove bad character,strength is same as previous entry
+                        {
+                            passwordStrength(text); //Check strength of password
+                        }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            { }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                pKeyCheck = false;
+            }
+        };
+        unameInput.addTextChangedListener(unInputTextWatcher); //Add the TextWatchers to the elements
+        passInput.addTextChangedListener(pInputTextWatcher);
+        //If TextWatcher is not picking up all hardware/software keyboard events - consider adding a onKeyListener to the view in addition (as that can pickup some but not all)
 
     }
 
+    /* FUNCTION INFORMATION
+     * NAME - exportSet
+     * INPUTS - view (tied to the radio group the method checks)
+     * OUTPUTS - none
+     * PURPOSE - This is the onClick event handler for the radio group that concerns the export settings of a user
+     */
     public void exportSet (View view)
     {
-        boolean checked = ((RadioButton) view).isChecked();
+        boolean checked = ((RadioButton) view).isChecked(); //Find out if the button was checked
 
         // Check which radio button was clicked
         switch (view.getId())
@@ -77,7 +201,7 @@ public class AccountCreation extends Activity {
             case R.id.acExportSettOne:
                 if (checked)
                 {
-                    exportValue = (String) (((RadioButton) view).getText());
+                    exportValue = (String) (((RadioButton) view).getText()); //Set the value to the text of the button
                 }
                 break;
             case R.id.acExportSettTwo:
@@ -98,16 +222,23 @@ public class AccountCreation extends Activity {
         }
     }
 
+    /* FUNCTION INFORMATION
+     * NAME - createAccount
+     * INPUTS - View (the button that acts as the submit button)
+     * OUTPUTS - none
+     * PURPOSE - This is the function to collate all information a user has provided and submitting it to the system to be converted into an account
+     */
     public void createAccount (View view)
     {
+        boolean created = false;
         //Get all the values that the user may have set
         boolean notBox = notInput.isChecked();
-        String gameValue = (gameInput.getSelectedItem()).toString();
-        String unameValue = (unameInput.getText()).toString();
+        String gameValue = (gameInput.getSelectedItem()).toString(); //We don't check this since it has a default value that is selected
+        String unameValue = (unameInput.getText()).toString(); //This two we need to check in case they weren't answered
         String passValue = (passInput.getText()).toString();
         String notValue = "";
 
-        //Translate notification check box to a string value
+        //Translate notification check box to a string value - this can only be yes or no (if user doesn't answer, default it NO)
         if(notBox == true)
         {
             notValue = "Yes";
@@ -117,25 +248,150 @@ public class AccountCreation extends Activity {
             notValue = "No";
         }
 
-        // Creating alert Dialog with one Button
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(AccountCreation.this);
-
-        // Setting Dialog Title
-        alertDialog.setTitle("Form Submission Results");
-
-        // Setting Dialog Message
-        alertDialog.setMessage("This is what you submitted\n" + "Username: " + unameValue + "\nPassword: " + passValue + "\nGamification Settings: " + gameValue + "\nData Export Settings: " + exportValue + "\nNotification Settings: " + notValue);
-
-        // Setting the finished button
-        alertDialog.setNegativeButton("OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
+        //Do a rolling check for empty username/password
+        if(unameValue.equals(""))
+        {
+            unameInput.requestFocus();
+            unameInput.setError("Please provide a username");
+        }
+        else
+        {
+            if(passValue.equals(""))
+            {
+                passInput.requestFocus();
+                passInput.setError("Please provide a password");
+            }
+            else
+            {
+                if(passwordStrength(passValue) < 3 )
+                {
+                    passInput.requestFocus();
+                    passInput.setError("Please provide a password with a strength of at least \"good\"");
+                }
+                else
+                {
+                    if(exportValue == null) //Need to select an export setting - no default
+                    {
+                        exportText.requestFocus();
+                        exportText.setError("Please select an export type");
                     }
-                });
+                    else
+                    {
+                        created = true;
+                    }
+                }
+            }
+        }
 
-        // Showing Alert Message
-        alertDialog.show();
+        //TODO - DELETE
+        if(created == true)
+        {
+            // Creating alert Dialog with one Button
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(AccountCreation.this);
+
+            // Setting Dialog Title
+            alertDialog.setTitle("Form Submission Results");
+
+            // Setting Dialog Message
+            alertDialog.setMessage("This is what you submitted\n" + "Username: " + unameValue + "\nPassword: " + passValue + "\nGamification Settings: " + gameValue + "\nData Export Settings: " + exportValue + "\nNotification Settings: " + notValue);
+
+            // Setting the finished button
+            alertDialog.setNegativeButton("OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+            // Showing Alert Message
+            alertDialog.show();
+        }
+
+        //TODO - ADD IN VALIDATE CALLS
+        //Send username/password to validate
+        //Make errors if they failed
+        //Send all as a package to writer
+        //Move to logged in activity
+
+    }
+
+    /* FUNCTION INFORMATION
+     * NAME - passwordStrength
+     * INPUTS - password (the string we need to check for complexity)
+     * OUTPUTS - none
+     * PURPOSE - This is the function that uses a simple method of password strength testing so that the user creates a moderately strong password
+     */
+    private int passwordStrength(String password)
+    {
+        int strength = 0;
+
+        if(password.length() < 10)
+        {
+            passInput.requestFocus(); //For when submit is pressed
+            passInput.setError("Password must be at least 10 characters long"); //Tell user why is is not valid
+
+            //Now display strength to user
+            passStrength.setText(("Strength: WEAK"));
+            strengthBar.setProgress(33);
+            (strengthBar.getProgressDrawable()).setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN); //May set all progress bars to this colour - integration will need testing to confirm
+        }
+        else
+        {
+
+            passInput.setError(null); //Since we have now submitted a large enough value - remove red ! since it's not incorrect now
+
+            if(password.matches(".*[a-z].*"))
+            {
+                strength++;
+            }
+
+            if(password.matches(".*[A-Z].*"))
+            {
+                strength++;
+            }
+
+            if(password.matches(".*[0-9].*"))
+            {
+                strength++;
+            }
+
+            if(password.length() > 14)
+            {
+                strength++;
+            }
+
+            switch (strength)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    //Now display strength to user
+                    passStrength.setText(("Strength: WEAK"));
+                    strengthBar.setProgress(33);
+                    (strengthBar.getProgressDrawable()).setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN); //May set all progress bars to this colour - integration will need testing to confirm
+                    break;
+                case 3:
+                    //Now display strength to user
+                    passStrength.setText(("Strength: GOOD"));
+                    strengthBar.setProgress(66);
+                    (strengthBar.getProgressDrawable()).setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN); //May set all progress bars to this colour - integration will need testing to confirm
+                    break;
+                case 4:
+                    //Now display strength to user
+                    passStrength.setText(("Strength: EXCELLENT"));
+                    strengthBar.setProgress(100);
+                    (strengthBar.getProgressDrawable()).setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN); //May set all progress bars to this colour - integration will need testing to confirm
+                    break;
+                default:
+                    //Now display strength to user
+                    passStrength.setText(("Strength: WEAK"));
+                    strengthBar.setProgress(33);
+                    (strengthBar.getProgressDrawable()).setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN); //May set all progress bars to this colour - integration will need testing to confirm
+            }
+        }
+
+        return strength;
+
     }
 
 }
